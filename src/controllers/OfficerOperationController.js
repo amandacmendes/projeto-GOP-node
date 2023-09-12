230b59ea623d6614483b303855ec151bf91a3e06
Amanda Mendes <83428587+amandacmendes@users.noreply.github.com>
2023-09-11 23:31:24 -0400

    Create OfficerOperationController and endpoints

---
+++ src/controllers/OfficerOperationController.js
@@ -0,0 +1,158 @@
+const { OfficerOperationModel } = require('../app/models/officeroperation');
+const { HttpHelper } = require('../utils/http-helper');
+
+class OfficerOperationController {
+
+    async create(request, response) {
+        try {
+
+            const {
+                officer_id,
+                operation_id
+            } = request.body;
+
+            // Cria link entre Oficial e Operação Policial
+            const officer_operation = await OfficerOperationModel.create({
+                officer_id,
+                operation_id
+            });
+
+            return response.status(201).json(officer_operation);
+
+        } catch (error) {
+            return response.status(500).json({
+                error: `Erro interno: ${error}`
+            })
+        }
+    }
+
+    async getAll(request, response) {
+        try {
+
+            const officer_operations = await OfficerOperationModel.findAll()
+                .then((data) => {
+                    if (data) {
+                        return response.status(200).json(data);
+                    } else {
+                        return response.status(204).json({ msg: "Não existem dados cadastrados." });
+                    }
+                });
+        } catch (error) {
+            return response.status(500).json({
+                error: `Erro interno: ${error}`
+            });
+        }
+    }
+
+    /*
+    '/officeroperation/officer/:ofid'    
+    */
+    async getAllFromOfficer(request, response) {
+        try {
+            const { ofid } = request.params;
+
+            const officer_operations = await OfficerOperationModel.findAll({
+                where: { officer_id: ofid }
+            }).then((data) => {
+                if (data) {
+                    return response.status(200).json(data);
+                } else {
+                    return response.status(204).json({ msg: "Não existem dados cadastrados." });
+                }
+            });
+        } catch (error) {
+            return response.status(500).json({
+                error: `Erro interno: ${error}`
+            });
+        }
+    }
+
+    /*
+    '/officeroperation/operation/:oid'
+    */
+    async getAllFromOperation(request, response) {
+        try {
+            const { oid } = request.params;
+
+            const officer_operations = await OfficerOperationModel.findAll({
+                where: { operation_id: oid }
+            }).then((data) => {
+                if (data) {
+                    return response.status(200).json(data);
+                } else {
+                    return response.status(204).json({ msg: "Não existem dados cadastrados." });
+                }
+            });
+        } catch (error) {
+            return response.status(500).json({
+                error: `Erro interno: ${error}`
+            });
+        }
+    }
+
+    async delete(request, response) {
+
+        const httpHelper = new HttpHelper(response);
+
+        try {
+
+            const { ofid, oid } = request.params;
+
+            if (!oid || !ofid) return httpHelper.badRequest('Parâmetros inválidos!');
+
+            const officer_operation_Exists = await OfficerOperationModel.findOne({
+                where: { officer_id: ofid, operation_id: oid }
+            });
+
+            if (!officer_operation_Exists) return httpHelper.notFound('Registro não existe no sistema!');
+
+            await OfficerOperationModel.destroy({ where: { officer_id: ofid, operation_id: oid } });
+
+            return httpHelper.ok({
+                message: 'Operação desvinculada de policial com sucesso!'
+            })
+        } catch (error) {
+            return httpHelper.internalError(`Erro interno: ${error}`);
+        }
+    }
+
+    async update(request, response) {
+
+        const httpHelper = new HttpHelper(response);
+
+        try {
+
+            const { ofid, oid } = request.params;
+
+            const {
+                officer_id,
+                operation_id
+            } = request.body;
+
+            if (!id) return httpHelper.badRequest('Parâmetros inválidos!');
+
+            const officer_operation_Exists = await OfficerOperationModel.findOne({
+                where: { officer_id: ofid, operation_id: oid }
+            });
+
+            if (!officer_operation_Exists) return httpHelper.notFound('Registro não existe no sistema!');
+
+            await OfficerOperationModel.update({
+                officer_id,
+                operation_id
+            }, {
+                where: { officer_id: officer_id, operation_id: operation_id }
+            });
+
+            return httpHelper.ok({
+                message: 'Operação atualizada com sucesso!'
+            });
+
+        } catch (error) {
+            return httpHelper.internalError(`Erro interno: ${error}`);
+        }
+    }
+
+}
+
+module.exports = { OfficerOperationController }
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ const { OfficerController } = require('./controllers/OfficerController');
 const { UserController } = require('./controllers/UserController');
 const { TeamController } = require('./controllers/TeamController');
 const { OperationController } = require('./controllers/OperationController');
+const { OfficerOperationController } = require('./controllers/OfficerOperationController');
 
 const routes = Router();
 
@@ -13,6 +14,7 @@ const userController = new UserController();
 const officerController = new OfficerController();
 const teamController = new TeamController();
 const operationController = new OperationController();
+const officerOperationController = new OfficerOperationController();
 
 // Users
 routes.post('/signup', userController.signup);
@@ -35,6 +37,14 @@ routes.post('/operation', authMiddleware, operationController.create);
 routes.get('/operation', authMiddleware, operationController.getAll);
 routes.delete('/operation/:id', authMiddleware, operationController.delete);
 routes.put('/operation/:id', authMiddleware, operationController.update);
+
+// OfficerOperation
+routes.post('/officeroperation', authMiddleware, officerOperationController.create);
+routes.get('/officeroperation', authMiddleware, officerOperationController.getAll);
+routes.delete('/officer/:ofid/operation/:oid', authMiddleware, officerOperationController.delete);
+routes.put('/officer/:ofid/operation/:oid', authMiddleware, officerOperationController.update);
+routes.get('/officeroperation/officer/:ofid', authMiddleware, officerOperationController.getAllFromOfficer);
+routes.get('/officeroperation/operation/:oid', authMiddleware, officerOperationController.getAllFromOperation);
 
 
 module.exports = { routes };
