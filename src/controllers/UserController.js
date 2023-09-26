a87ada7bb01abeeb3fcfc3439777ed86758f77b2
Amanda Mendes <83428587+amandacmendes@users.noreply.github.com>
2023-09-25 23:01:44 -0400

    user update password hash

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -51,7 +51,7 @@ class UserController {
             );
 
             return response.status(200).json({
-                accessToken, 
+                accessToken,
                 id: userExists.id
             });
 
@@ -195,8 +195,11 @@ class UserController {
             const userExists = await UserModel.findByPk(id);
             if (!userExists) return response.status(204).json({ msg: "Não existem dados cadastrados." });
 
+            // Criptografa senha
+            const passwordHashed = await bcrypt.hash(password, SALT);
+
             await UserModel.update({
-                email, password, status, officer_id
+                email, password: passwordHashed, status
             }, {
                 where: { id }
             });
@@ -207,56 +210,6 @@ class UserController {
 
         } catch (error) {
             return httpHelper.internalError(`Erro interno: ${error}`);
-        }
-    }
-
-
-
-
-    async register(request, response) {
-        const httpHelper = new HttpHelper(response);
-        try {
-            const { email, password } = request.body;
-            if (!email || !password) return httpHelper.badRequest('E-mail e senha são obrigatórios!');
-            const userAlreadyExists = await UserModel.findOne({ where: { email } });
-            if (userAlreadyExists) return httpHelper.badRequest('E-mail de usuário já cadastrado!');
-            const passwordHashed = await bcrypt.hash(
-                password,
-                Number(process.env.SALT)
-            );
-            const user = await UserModel.create({
-                email,
-                password: passwordHashed,
-            });
-            if (!user) return httpHelper.badRequest('Houve um erro ao criar usuário');
-            const accessToken = jwt.sign(
-                { id: user.id },
-                process.env.TOKEN_SECRET,
-                { expiresIn: process.env.TOKEN_EXPIRES_IN }
-            );
-            return httpHelper.created({ accessToken });
-        } catch (error) {
-            return httpHelper.internalError(error);
-        }
-    }
-
-    async login(request, response) {
-        const httpHelper = new HttpHelper(response);
-        try {
-            const { email, password } = request.body;
-            if (!email || !password) return httpHelper.badRequest('E-mail e senha são obrigatórios!');
-            const userExists = await UserModel.findOne({ where: { email } });
-            if (!userExists) return httpHelper.notFound('Usuário não encontrado!');
-            const isPasswordValid = await bcrypt.compare(password, userExists.password);
-            if (!isPasswordValid) return httpHelper.badRequest('Senha incorreta!');
-            const accessToken = jwt.sign(
-                { id: userExists.id },
-                process.env.TOKEN_SECRET,
-                { expiresIn: process.env.TOKEN_EXPIRES_IN }
-            );
-            return httpHelper.ok({ accessToken });
-        } catch (error) {
-            return httpHelper.internalError(error);
         }
     }
 
